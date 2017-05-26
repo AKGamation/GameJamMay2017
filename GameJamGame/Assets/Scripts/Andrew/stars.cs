8aea69318a5bc75cd92bf1921e034ca0005f215f
wdchris381275 <christopheraloney@hotmail.com>
2017-05-27 01:32:44 +1000

    ...

---
+++ GameJamGame/Assets/Scripts/Andrew/stars.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this will eventually make a bunch of stars
+public class stars : MonoBehaviour
+{
+    public int numberOfStars = 1000; // number of stars to spawn
+    public float distanceFromOrigin = 100.0f; // distance from the origin
+    public GameObject starObject; // star model
+    public List<Color> colorRange = new List<Color>();
+
+	// Use this for initialization
+	void Start ()
+    {
+		for(int i = 0; i < numberOfStars; i++)
+        {
+            Vector3 newLocation = Random.onUnitSphere * distanceFromOrigin;
+            GameObject currentStar = Instantiate(starObject, newLocation, Quaternion.identity) as GameObject;
+            currentStar.GetComponent<MeshRenderer>().material.color = Color.Lerp(colorRange[0], colorRange[1], Random.Range(0.0f, 1.0f));
+        }
+	}
+}
---
+++ GameJamGame/Assets/Scripts/David/LightingController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingController : MonoBehaviour {
+
+
+    public GameObject button;
+    public bool lightState = true;
+
+    //need event from button or sensor
+
+    
+    
+    
+	// Use this for initialization
+	void Start ()
+    {
+    //   name of button script
+	//   button.GetComponent<buttscrnam>	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //if(button.buttonscript == true)
+        //{
+        //  lightstate = true;
+        //  GetComponent<Light>().enabled = false;
+        //{
+        //else
+        //{
+        //  lightstate = false
+        //  GetComponent<Light>().enabled = true;
+        //{
+
+        //testing
+        if(lightState == false)
+        {
+            GetComponent<Light>().enabled = false;
+        }
+        else if(lightState == true)
+        {
+            GetComponent<Light>().enabled = true;
+        }
+       
+
+
+    }
+
+    void ToggleLight()
+    {
+        if (lightState == true)
+        {
+            lightState = false;
+        }
+        else
+        {
+            lightState = true;
+        }
+    }
+
+
+
+
+
+}
