f9ae507d0c130a5dbf25772338b55651bf51470a
Darklight437 <darklight437@gmail.com>
2017-05-26 22:13:00 +1000

    Door collider + lighting controller (WIP)

--- GameJamGame/Assets/Scripts/David/LightingController.cs
+++ GameJamGame/Assets/Scripts/David/LightingController.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class LightingController : MonoBehaviour {
 
+
+    public GameObject button;
+    public bool lightState;
+
+    //need event from button or sensor
+
+    
+    
+    
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+    //   name of button script
+	//   button.GetComponent<buttscrnam>	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void ToggleLight()
+    {
+        if (lightState == true)
+        {
+            lightState = false;
+        }
+        else
+        {
+            lightState = true;
+        }
+    }
+
+
+
+
+
 }
