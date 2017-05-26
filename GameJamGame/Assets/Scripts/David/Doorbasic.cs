94de388c048e107eb605a1faefc78f6dbab8affb
Darklight437 <darklight437@gmail.com>
2017-05-26 21:51:11 +1000

    Door functionality and prefab
    door prefab requres two objects to move between

--- GameJamGame/Assets/Scripts/David/Doorbasic.cs
+++ GameJamGame/Assets/Scripts/David/Doorbasic.cs
@@ -7,19 +7,28 @@ public class Doorbasic : MonoBehaviour {
     public GameObject OpenPos;
     public GameObject ClosePos;
     public float speed;
-    bool open;
+    public bool open;
     float step;
     // Use this for initialization
     void Start ()
     {
-	    
+        
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         float step = speed * Time.deltaTime;
-        DoorMove();
+        if (open == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, OpenPos.transform.position, step);
+        }
+        else if (open == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, ClosePos.transform.position, step);
+        }
+
+        
 
     }
 
