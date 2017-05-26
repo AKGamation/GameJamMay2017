7880b5480a821b131a7b327d1920dae73cdd4e1c
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-05-26 19:07:15 +1000

    default assets
    - fps controller etc
    - made my scene
    - made stars script

---
+++ GameJamGame/Assets/Scripts/Andrew/stars.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this will eventually make a bunch of stars
+public class stars : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
