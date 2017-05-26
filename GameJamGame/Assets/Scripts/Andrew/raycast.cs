6c4991ce9c046e85b3a44f279ab4edc093109780
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-05-26 20:23:27 +1000

    gitignore

---
+++ GameJamGame/Assets/Scripts/Andrew/raycast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    public float playerReach; // how far the player can reach
+    Ray ray;
+    RaycastHit hitInfo;
+    private int layerMask = (LayerMask.NameToLayer("interactive"));
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // make ray
+        ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        // raycast
+        if(Physics.Raycast(ray, out hitInfo, playerReach, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (Input)
+        }
+	}
+}
