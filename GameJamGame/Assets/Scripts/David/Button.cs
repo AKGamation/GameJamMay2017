ed143007c8a900821f942dcc575e0bf45d3dc7e6
Darklight437 <darklight437@gmail.com>
2017-05-26 20:31:13 +1000

    Adding  test scene and scripts

---
+++ GameJamGame/Assets/Scripts/David/Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+
+    public bool ButtonState;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+       	
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+        
+        
+        
+	}
+
+    int color()
+    {
+        return 0;
+    }
+    
+
+    
+
+
+}
